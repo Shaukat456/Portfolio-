4b9d5d390aabbc05493462198239da2face781ff
Shaukat <sohailriazz267@gmail.com>
2021-01-06 20:09:02 +0500

    Added Intro

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import { Slide, LightSpeed,Rotate} from 'react-reveal';
-import { Fade } from 'react-bootstrap';
+
 
 
 const Intro=()=>{
     return(
         <>
-        <Slide>
+        
           <div className="container">
             <section className="about-me" id='aboutt'>           
                  <div className="intro mx-auto">
-                        <Slide>   <h2>Shaukat Sohail</h2>
-                            <p>Mern Stack Developer</p></Slide>  
-                             <a href="./Resume/Resume.pdf" target="_sh"> <button
-                                    className="cv-btn">RESUME</button></a>
-                                   <a href="#porfolio"> <button className="work-btn">MY WORK</button></a> 
+                       <h2>Shaukat Sohail</h2> 
+                        <div className="anim">
+                            <p>Mern Stack Developer</p> <br/>
+                           
+                           
+                            <p>Full Web Stack Developer</p> <br/>
+                           
+                            
+                            <p>React Developer</p><br/>
+                        </div>
+                                       <a href="./Resume/Resume.pdf" target="_sh"> <Slide right> <button
+                                    className="cv-btn">RESUME </button></Slide></a>
+                                   <a href="#porfolio"> <button className="work-btn">MY WORK </button> </a> 
                             
                         </div>
                   
                     </section>
                    </div>
-                   </Slide>
         
 
 
